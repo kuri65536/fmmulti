c92cf7015fa13c4b355f33a70129c3c99d3b326b
Shimoda <kuri65536@hotmail.com>
2019-10-27 12:56:14 +0900

    options: implement arguments.

--- fmmulti.py
+++ fmmulti.py
@@ -1,30 +1,72 @@
 #! env python3
 from argparse import ArgumentParser
 from enum import Enum
+import logging
+from logging import debug as debg
 import tempfile
-from typing import Dict, List, Text
+import os
 import sys
+from typing import Dict, List, Text
 from zipfile import ZipFile
 
 
 class runmode(Enum):  # {{{1
     normal = 1
 
+    @classmethod  # choices {{{1
+    def choices(cls) -> List[Text]:
+        ret = []
+        for i in runmode:
+            t = Text(i)
+            t = t.replace("runmode.", "")
+            ret.append(t)
+        return ret
+
+    @classmethod  # parse {{{1
+    def parse(cls, src: Text) -> 'runmode':
+        for i in runmode:
+            if src == Text(i):
+                return i
+        raise KeyError("'{}' is not a mode in {}".format(src, cls.choices()))
+
 
 class options(object):  # {{{1
     def __init__(self) -> None:  # {{{1
         self.fname_xml = ""
         self.fname_zip = ""
+        self.fname_out = ""
         self.mode = runmode.normal
 
     @classmethod  # parser {{{1
     def parser(cls) -> ArgumentParser:  # {{{1
         arg = ArgumentParser()
+        arg.add_argument("-o", "--output", default="")
+        arg.add_argument("-m", "--mode", choices=runmode.choices(),
+                         default=Text(runmode.normal))
+        arg.add_argument("-i", "--input-xml", default="")
+        arg.add_argument("input_zip_name", type=Text, nargs="?")
         return arg
 
     @classmethod  # parse {{{1
     def parse(cls, args: List[Text]) -> 'options':
+        logging.basicConfig(level=logging.DEBUG)
         ret = options()
+        opts = ret.parser().parse_args(args)
+        ret.fname_out = opts.output
+        ret.fname_xml = opts.input_xml
+        ret.mode = runmode.parse(opts.mode)
+        src = ret.fname_zip = opts.input_zip_name
+        if not isinstance(src, Text):
+            src = ""
+        if len(src) < 1:
+            src = ret.fname_xml
+        if len(src) < 1:
+            ret.fname_out = "/dev/stdout"
+        else:
+            p = os.path.dirname(src)
+            src = os.path.basename(src)
+            src, ext = os.path.splitext(src)
+            ret.fname_out = os.path.join(p, src + "-1.mm")
         return ret
 
 
@@ -60,8 +102,15 @@ class FMXml(object):  # {{{1
 def zip_extract(fname: Text) -> Text:  # {{{1
     ret = tempfile.mktemp(".xml", "fmmulti", dir=".")
     try:
+        debg("zip:" + fname)
         zf = ZipFile(fname)
+    except Exception as ex:
+        debg("zip:" + fname + "->" + Text(ex))
+        return ""
+
+    try:
         for zi in zf.infolist():
+            debg("found entry {}".format(zi))
             if zi.is_dir():
                 continue
             with open(ret, "w") as fp:
